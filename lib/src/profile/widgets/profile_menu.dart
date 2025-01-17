6dd2777df2c182e2cec273ed4bcffd87d06d9d54
sakib75sh <sakibhasan75sh@gmail.com>
2025-01-17 16:50:54 +0600

    nothing

--- lib/src/profile/widgets/profile_menu.dart
+++ lib/src/profile/widgets/profile_menu.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+
 import 'package:ticket_app/styles/styles.dart';
 
 class ProfileMenu extends StatelessWidget {
--- lib/src/profile/widgets/profile_pic.dart
+++ lib/src/profile/widgets/profile_pic.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ticket_app/styles/media_path.dart';
+
 import 'package:ticket_app/styles/styles.dart';
 
 class ProfilePic extends StatelessWidget {
